e400189e6e16c6926b99787d0d356e975633a568
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-01-30 01:38:28 -0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -2,18 +2,13 @@ require('dotenv').config();
 import express, { Application } from 'express';
 import { connect } from 'mongoose';
 
-// API routing:
 import router from './router';
 
 const app: Application = express();
 
-const DEFAULT_PORT = 4000;
-const DEFAULT_HOST = 'localhost';
-const DEFAULT_URI = '';
-
-const PORT: number = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : DEFAULT_PORT;
-const HOST: string = process.env.SERVER_HOST ? process.env.SERVER_HOST : DEFAULT_HOST;
-const URI: string = process.env.DB_URI ? process.env.DB_URI : DEFAULT_URI;
+const PORT = <string>process.env.SERVER_PORT;
+const HOST = <string>process.env.SERVER_HOST;
+const URI = <string>process.env.DB_URI;
 
 app.use(router);
 
