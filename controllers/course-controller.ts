378fec39941dfdda4a72bbc69439b82881af8a5b
Vitaliy Malyshev <zich.contemporary@mail.ru>
2022-02-11 12:04:33 -0700

    Update Course model and controller
    Delete the second subcategory.
    
    Remove the 'update' and 'delete' functions since the logic has
    been moved to a single query that deletes, updates, and adds
    objects in the database.

--- controllers/course-controller.ts
+++ controllers/course-controller.ts
@@ -4,7 +4,19 @@ import { Course, CourseModel } from './../models/course';
 class CourseController {
   public async create(request: Request, response: Response): Promise<void> {
     try {
-      await CourseModel.create(request.body);
+      const {courses, deletedCourses}: {courses: Course[], deletedCourses: string[]} = request.body;
+      
+      courses.forEach(async (course: Course) => {
+        let courseIsFounded = await CourseModel.findByIdAndUpdate(course._id, course);
+        if (courseIsFounded === null) {
+          await CourseModel.create(course);
+        }
+      });
+      
+      deletedCourses.forEach(async (deletedCourse: string) => {
+        await CourseModel.findByIdAndDelete(deletedCourse);
+      })
+      
       response.sendStatus(200);
     } catch (error) {
       response.status(500).json(error);
@@ -13,7 +25,6 @@ class CourseController {
 
   public async read(request: Request, response: Response): Promise<void> {
     try {
-      response.set('Access-Control-Allow-Origin', `http://localhost:3000`);
       await CourseModel.find(request.query).lean().exec(
         (error: any, data: Course[]): void => {
           if (error) {
@@ -22,25 +33,6 @@ class CourseController {
           response.status(200).json(data);
         }
       );
-    } catch (error) {
-      response.status(500).json(error);
-    }
-  }
-
-  public async update(request: Request, response: Response): Promise<void> {
-    try {
-      const updatedCourses: Course[] = request.body.update;
-      updatedCourses.map( async (course) => await CourseModel.updateOne({ _id: course._id }, course));
-      response.sendStatus(200);
-    } catch (error) {
-      response.status(500).json(error);
-    }
-  }
-
-  public async delete(request: Request, response: Response): Promise<void> {
-    try {
-      await CourseModel.deleteOne(request.body);
-      response.sendStatus(200);
     } catch (error) {
       response.status(500).json(error);
     }
--- models/course.ts
+++ models/course.ts
@@ -4,23 +4,13 @@ export interface Course {
   readonly _id: ObjectId;
   readonly name: string;
   readonly price?: number; 
-  readonly subcategory?: Subcategory[];
-}
-
-interface Subcategory {
-  readonly name: string;
   readonly subcategory?: string[];
 }
-
-const SubcategorySchema = new Schema<Subcategory> ({
-  name: { type: String, unique: false, required: true, minlength: 1, maxlength: 64 },
-  subcategory: { type: [String], unique: false, required: false, minlength: 1, maxlength: 64 }
-});
 
 const CourseSchema = new Schema<Course> ({
   name: { type: String, unique: false, required: true, minlength: 1, maxlength: 64 },
   price: { type: Number, unique: false, required: false, min: 0, max: 1000000 },
-  subcategory: { type: [SubcategorySchema], unique: false, required: false }
+  subcategory: { type: [String], unique: false, required: false, minlength: 1, maxlength: 64 }
 });
 
 export const CourseModel = model('Course', CourseSchema);
